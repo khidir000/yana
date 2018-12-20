abf5bc63da85bf9180c9a14fd4559564987d852b
kediridev <kwira04@gmail.com>
2018-12-20 14:47:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kediridev/yana/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kediridev.yana
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
